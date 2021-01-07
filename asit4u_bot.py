109300347409539b5feb0de5eccf7fbe47d7bdfd
Pieter Reyneke <pieter@asit4u.co.za>
2021-01-07 14:02:36 +0200

    init commit

---
+++ asit4u_bot.py
@@ -0,0 +1,19 @@
+import discord
+import myConfig
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!' + ' ' + message.author.display_name + '! What would you like for lunch?')
+
+
+client.run(myConfig.bot_token)
